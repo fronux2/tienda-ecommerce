f2d79b89ed3462b01bb81983a3a8fb7e4721e54f
marcos <marcosantonio.munozg@gmail.com>
2025-10-21 00:37:31 -0300

    agregue redireccion a admin/mangas

--- src/app/admin/mangas/page.tsx
+++ src/app/admin/mangas/page.tsx
@@ -1,7 +1,28 @@
 import Loading from "@/components/Loading";
 import MangaDataLoader from "@/components/MangaDataLoader";
 import { Suspense } from "react";
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 export default async function Page() {
+  const supabase = await createClient()
+    
+    const { data: { user } } = await supabase.auth.getUser()
+  
+    if (!user) {
+      redirect('/login')
+      return
+    }
+  
+    const { data: perfil } = await supabase
+      .from('usuarios')
+      .select('rol_id')
+      .eq('id', user.id)
+      .single()
+  
+    if (perfil?.rol_id !== 2) {
+      redirect('/login')
+      return
+    }
   return (
     <Suspense fallback={<Loading />}>
       <main className="p-8">
